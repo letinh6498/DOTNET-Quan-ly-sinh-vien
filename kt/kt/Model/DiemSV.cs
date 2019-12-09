30ed9e14581aed33c88c65f6a8fe15fa7d229a9f
le phu tinh <40222436+tinh-le@users.noreply.github.com>
2019-12-09 15:48:30 +0700

    C#

---
+++ kt/kt/Model/DiemSV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kt.Model
+{
+    public class DiemSV
+    {
+        private String maSV;
+        private String maMH;
+        private int diem;
+
+        public DiemSV()
+        {
+        }
+
+        public DiemSV(DataRow row)
+        {
+            this.MaSV = row["maSV"].ToString();
+            this.MaMH = row["maMonHoc"].ToString();
+            this.Diem = Convert.ToInt32(row["diem"].ToString());
+        }
+
+        public string MaSV { get => maSV; set => maSV = value; }
+        public string MaMH { get => maMH; set => maMH = value; }
+        public int Diem { get => diem; set => diem = value; }
+       
+    }
+}
